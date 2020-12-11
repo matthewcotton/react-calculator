4a5e59583487239d064b4336af13f517d339c8fa
matthewcotton <matthewneilcotton@gmail.com>
2020-12-11 15:16:16 +0000

    Refactored code and added hsitory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import Container from "react-bootstrap/Container";
-//import Col from "react-bootstrap/Col";
-//import Row from "react-bootstrap/Row";
-//import Card from "react-bootstrap/Card";
-//import Button from "react-bootstrap/Button";
 import "./App.css";
-import Buttons from "./Buttons.js"; 
+import Buttons from "./Buttons.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-  
-    };
+    this.state = {};
   }
 
   render() {
-    return (
-        <Buttons></Buttons>
-    );
+    return <Buttons></Buttons>;
   }
 }
 
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
@@ -9,40 +10,67 @@ class Buttons extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentSelection: "",
+      outputLine: "",
       resultHistory: [],
     };
   }
 
-  // Add number or operator to currentSelection
-  addNumOrOp(char) {
-    // Check if char is a number and the last result is still the currentSelection
+  // Add number or operator to outputLine
+  pushInput(char) {
+    // Check if result history is currently being shown
+    if (this.state.outputLine.includes("Result History")) {
+      this.clear();
+      console.log("test");
+      return;
+    }
+
+    // If char is "clear"
+    if (char === "clear") {
+      this.clear();
+    }
+    // If char is "="
+    else if (char === "=") {
+      this.calculate();
+    }
+    // If char is "plus-minus"
+    else if (char === "plus-minus") {
+      this.changeSign();
+    }
+    // If char is "history"
+    else if (char === "history") {
+      this.history();
+    }
+    // Else
+    else {
+      this.addToOutput(char);
+    }
+  }
+
+  // Add to output line
+  addToOutput(char) {
+    // Check if char is a number and the last result is still the outputLine and not equal to whole history
     if (
-      this.state.currentSelection ===
+      this.state.outputLine ===
         this.state.resultHistory[this.state.resultHistory.length - 1] &&
       !isNaN(char)
     ) {
       this.clear();
     }
 
-    // Check if first addition to string is an operator
     var re = /[+\-*/]/g;
-
-    if (char.match(re) && this.state.currentSelection === "") {
+    // Check if first addition to string is an operator
+    if (char.match(re) && this.state.outputLine === "") {
       return;
     }
     // If char is an operator and the last item in the string is an operator replace the last value
     else if (
       char.match(re) &&
-      this.state.currentSelection[this.state.currentSelection.length - 1].match(
-        re
-      )
+      this.state.outputLine[this.state.outputLine.length - 1].match(re)
     ) {
       this.setState((state) => {
         return {
-          currentSelection:
-            state.currentSelection.slice(0, state.currentSelection.length - 1) +
-            char,
+          outputLine:
+            state.outputLine.slice(0, state.outputLine.length - 1) + char,
         };
       });
     }
@@ -50,21 +78,28 @@ class Buttons extends React.Component {
     else {
       this.setState((state) => {
         return {
-          currentSelection: state.currentSelection + char,
+          outputLine: state.outputLine + char,
         };
       });
     }
   }
 
-  // Evalulate the currentSelection string
+  // Evalulate the outputLine string
   calculate() {
-    var equation = this.state.currentSelection;
-
+    var equation = this.state.outputLine;
+    var re = /[+\-*/.]/g;
+    // If equation only includes a decimal place is an empty string
+    if (equation === "." || equation === "") {
+      return;
+    }
+    else if (equation[0] === "." && equation[1].match(re)) {
+      this.clear();
+      return;
+    }
     // Check string is only contains valid characters
     equation = equation.replace(/[^-()\d/*+.]/g, "");
 
-    var re = /[+\-*/.]/g;
-
+    
     // Remove unwanted operators and decimal places at the end of the string
     while (equation[equation.length - 1].match(re)) {
       equation = equation.slice(0, equation.length - 1);
@@ -80,7 +115,7 @@ class Buttons extends React.Component {
           equation = "";
           this.setState(() => {
             return {
-              currentSelection: "",
+              outputLine: "",
             };
           });
         }
@@ -94,18 +129,18 @@ class Buttons extends React.Component {
 
       this.setState((state) => {
         return {
-          currentSelection: String(answer),
+          outputLine: String(answer),
           resultHistory: state.resultHistory.concat(String(answer)),
         };
       });
     }
   }
 
-  // Clear the currentSelection
+  // Clear the outputLine
   clear() {
     this.setState(() => {
       return {
-        currentSelection: "",
+        outputLine: "",
       };
     });
   }
@@ -113,20 +148,93 @@ class Buttons extends React.Component {
   // Change sign if only one number is in the string
   changeSign() {
     var re = /[+\-*/]/g;
-    var nums = this.state.currentSelection.split(re);
+    var nums = this.state.outputLine.split(re);
     if (nums.length === 1) {
       this.setState(() => {
         return {
-          currentSelection: "-" + nums[0],
+          outputLine: "-" + nums[0],
         };
       });
     } else if (nums.length === 2 && nums[0] === "") {
       this.setState(() => {
         return {
-          currentSelection: nums[1],
+          outputLine: nums[1],
         };
       });
     }
+  }
+
+  history() {
+    this.setState((state) => {
+      return {
+        outputLine: "Result History: " + state.resultHistory.join(" | "),
+      };
+    });
+  }
+
+  // Populate buttons
+  renderButtonBlock(rowNum) {
+    const buttons = [
+      [
+        ["7", "seven", "7", "full-width", ""],
+        ["8", "eight", "8", "full-width", ""],
+        ["9", "nine", "9", "full-width", ""],
+        ["÷", "divide", "/", "full-width", ""],
+      ],
+      [
+        ["4", "four", "4", "full-width", ""],
+        ["5", "five", "5", "full-width", ""],
+        ["6", "six", "6", "full-width", ""],
+        ["x", "multiply", "*", "full-width", ""],
+      ],
+      [
+        ["1", "one", "1", "full-width", ""],
+        ["2", "two", "2", "full-width", ""],
+        ["3", "three", "3", "full-width", ""],
+        ["-", "minus", "-", "full-width", ""],
+      ],
+      [
+        ["±", "plus-minus", "plus-minus", "full-width", ""],
+        ["0", "zero", "0", "full-width", ""],
+        [".", "point", ".", "full-width", ""],
+        ["+", "plus", "+", "full-width", ""],
+      ],
+      [["=", "equals", "=", "full-width full-height", "2"]],
+      [
+        ["Clear", "clear", "clear", "full-width", "6"],
+        ["Result History", "history", "history", "full-width", "6"],
+      ],
+    ];
+    var buttonBlock = [];
+
+    buttons[rowNum].forEach((button) => {
+      const newButton = this.createButton(
+        button[0],
+        button[1],
+        button[2],
+        button[3],
+        button[4]
+      );
+      buttonBlock.push(newButton);
+    });
+
+    return buttonBlock;
+  }
+
+  // Create a new button
+  createButton(displayValue, id, calcValue, className, colWidth) {
+    return (
+      <Col xs={colWidth}>
+        <Button
+          variant="info"
+          id={id}
+          className={className}
+          onClick={() => this.pushInput(calcValue)}
+        >
+          <h1> {displayValue} </h1>
+        </Button>
+      </Col>
+    );
   }
 
   render() {
@@ -137,7 +245,7 @@ class Buttons extends React.Component {
           <Col className="text-right">
             <Card>
               <Card.Body id="results-box">
-                <h1> {this.state.currentSelection} </h1>
+                <h1> {this.state.outputLine} </h1>
               </Card.Body>
             </Card>
           </Col>
@@ -145,186 +253,23 @@ class Buttons extends React.Component {
         <br />
         <Card>
           <Card.Body>
-            <Row>
+            <Row className="justify-content-between">
               <Col xs={10}>
                 <Container>
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="seven"
-                        onClick={() => this.addNumOrOp("7")}
-                      >
-                        <h1>7</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="eight"
-                        onClick={() => this.addNumOrOp("8")}
-                      >
-                        <h1>8</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="nine"
-                        onClick={() => this.addNumOrOp("9")}
-                      >
-                        <h1>9</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="divide"
-                        onClick={() => this.addNumOrOp("/")}
-                      >
-                        <h1>÷</h1>
-                      </Button>
-                    </Col>
-                  </Row>
+                  <Row>{this.renderButtonBlock(0)}</Row>
                   <br />
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="four"
-                        onClick={() => this.addNumOrOp("4")}
-                      >
-                        <h1>4</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="five"
-                        onClick={() => this.addNumOrOp("5")}
-                      >
-                        <h1>5</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="six"
-                        onClick={() => this.addNumOrOp("6")}
-                      >
-                        <h1>6</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="multiply"
-                        onClick={() => this.addNumOrOp("*")}
-                      >
-                        <h1>x</h1>
-                      </Button>
-                    </Col>
-                  </Row>
+                  <Row>{this.renderButtonBlock(1)}</Row>
                   <br />
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="one"
-                        onClick={() => this.addNumOrOp("1")}
-                      >
-                        <h1>1</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="two"
-                        onClick={() => this.addNumOrOp("2")}
-                      >
-                        <h1>2</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="three"
-                        onClick={() => this.addNumOrOp("3")}
-                      >
-                        <h1>3</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="minus"
-                        onClick={() => this.addNumOrOp("-")}
-                      >
-                        <h1>‒</h1>
-                      </Button>
-                    </Col>
-                  </Row>
+                  <Row>{this.renderButtonBlock(2)}</Row>
                   <br />
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="plus-minus"
-                        onClick={() => this.changeSign()}
-                      >
-                        <h1>±</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="zero"
-                        onClick={() => this.addNumOrOp("0")}
-                      >
-                        <h1>0</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="point"
-                        onClick={() => this.addNumOrOp(".")}
-                      >
-                        <h1>.</h1>
-                      </Button>
-                    </Col>
-                    <Col>
-                      <Button
-                        variant="info"
-                        id="plus"
-                        onClick={() => this.addNumOrOp("+")}
-                      >
-                        <h1>+</h1>
-                      </Button>
-                    </Col>
-                  </Row>
+                  <Row>{this.renderButtonBlock(3)}</Row>
                 </Container>
               </Col>
-              <Col xs={2}>
-                <Button
-                  variant="info"
-                  id="equals"
-                  onClick={() => this.calculate()}
-                >
-                  <h1>=</h1>
-                </Button>
-              </Col>
+              {this.renderButtonBlock(4)}
             </Row>
             <br />
             <Row className="justify-content-center">
-              <Button
-                variant="info"
-                id=""
-                className="clear"
-                onClick={() => this.clear()}
-              >
-                <h1>Clear</h1>
-              </Button>
+              {this.renderButtonBlock(5)}
             </Row>
           </Card.Body>
         </Card>
